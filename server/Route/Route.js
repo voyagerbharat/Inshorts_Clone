567d428124dc810e49328b6b5740ad4c1728a973
VoyagerBharat <monugera1@gmail.com>
2021-11-15 01:52:14 +0530

    added pagination and infinte scrolling

--- server/Route/Route.js
+++ server/Route/Route.js
@@ -1,3 +1,8 @@
 import express from "express";
+import { getNews } from "../Controller/news-controller.js";
 
 const route = express.Router();
+
+route.get("/news", getNews);
+
+export default route;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 import Connection from "./connection/db.js";
 
 import express from "express";
+import cors from "cors";
+import bodyparser from "body-parser";
 import DefaultData from "./default.js";
+import route from "./Route/Route.js";
 
 const app = express();
 
 const PORT = 8900;
 
 Connection();
+
+app.use(cors());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json({ extended: true }));
+app.use("/", route);
 
 app.listen(PORT, () => {
   console.log(`Server is ruuning on ${PORT}`);
